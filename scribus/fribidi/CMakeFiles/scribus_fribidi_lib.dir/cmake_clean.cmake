817539af6692fd383892c89fb37ef1d788647565
Mosaab Alzoubi <moceap@hotmail.com>
2015-06-23 11:25:19 +0300

    Implantation RTL support #3

---
+++ scribus/fribidi/CMakeFiles/scribus_fribidi_lib.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/scribus_fribidi_lib.dir/fribidi-arabic.c.o"
+  "CMakeFiles/scribus_fribidi_lib.dir/fribidi-joining-types.c.o"
+  "CMakeFiles/scribus_fribidi_lib.dir/fribidi-bidi.c.o"
+  "CMakeFiles/scribus_fribidi_lib.dir/fribidi-mem.c.o"
+  "CMakeFiles/scribus_fribidi_lib.dir/fribidi-bidi-types.c.o"
+  "CMakeFiles/scribus_fribidi_lib.dir/fribidi-mirroring.c.o"
+  "CMakeFiles/scribus_fribidi_lib.dir/fribidi.c.o"
+  "CMakeFiles/scribus_fribidi_lib.dir/fribidi-run.c.o"
+  "CMakeFiles/scribus_fribidi_lib.dir/fribidi-deprecated.c.o"
+  "CMakeFiles/scribus_fribidi_lib.dir/fribidi-shape.c.o"
+  "CMakeFiles/scribus_fribidi_lib.dir/fribidi-joining.c.o"
+  "libscribus_fribidi_lib.pdb"
+  "libscribus_fribidi_lib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/scribus_fribidi_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
