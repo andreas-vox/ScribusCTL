eaf47be857c53b32e791c97a209040f46389be39
Dawood Albadi <daawood.albadi@gmail.com>
2016-02-02 14:30:49 +0400

    Solve problem of ascent and decent in some fonts

--- scribus/pageitem_textframe.cpp
+++ scribus/pageitem_textframe.cpp
@@ -449,11 +449,11 @@ struct LineControl {
     void updateHeightMetrics(const StoryText& itemText)
     {
 		const CharStyle& cStyle(itemText.charStyle(line.firstChar));
-		double scaleV = cStyle.scaleV() / 1000.0;
-		double offset = (cStyle.fontSize() / 10) * (cStyle.baselineOffset() / 1000.0);
-		line.ascent = cStyle.font().ascent(cStyle.fontSize()/10.00) * scaleV + offset;
-		line.descent = cStyle.font().descent(cStyle.fontSize()/10.00) * scaleV - offset;
-#if 0
+//		double scaleV = cStyle.scaleV() / 1000.0;
+//		double offset = (cStyle.fontSize() / 10) * (cStyle.baselineOffset() / 1000.0);
+//		line.ascent = cStyle.font().ascent(cStyle.fontSize()/10.00) * scaleV + offset;
+//		line.descent = cStyle.font().descent(cStyle.fontSize()/10.00) * scaleV - offset;
+
 		double asce, desc;
         for (int zc = 0; zc < charsInLine; ++zc)
         {
@@ -479,7 +479,7 @@ struct LineControl {
             line.ascent  = qMax(line.ascent, asce);
             line.descent = qMax(line.descent, desc);
         }
-#endif
+
     }
 
 // yPos should not be changed when all line is already calculated - at new y position there can be overflow!!!
